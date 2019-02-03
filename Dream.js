3c11fa69cefb84199ef3d81a18527f8ccc9b2699
yousefmmm <47262455+yousefmmm@users.noreply.github.com>
2019-02-03 16:22:20 +0200

    Update Dream.js

--- Dream.js
+++ Dream.js
@@ -1,14 +1,39 @@
-﻿const Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
-console.log("Hassan_Gamer");
+const prefix = '.'
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`hqhqhqhqhqh`,)
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
 
 
-client.on("ready", () => {
-let channel =     client.channels.get("541618484174520330")
-setInterval(function() {
-channel.send(`Abram Ya zft`)
-}, 25)
-})
- 
- 
+
+
+
+
+
+
+
 client.login("NTI5MDcwODk1NzI2MTk4ODA0.DziEZQ.yIPPM-2wVlTiCec5kZb-dcqQsBU");
