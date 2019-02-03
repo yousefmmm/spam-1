a5165c6c1516b1417874c2b3cb6c333cae55d782
yousefmmm <47262455+yousefmmm@users.noreply.github.com>
2019-02-03 15:53:13 +0200

    Add files via upload

---
+++ Dream.js
@@ -0,0 +1,14 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Hassan_Gamer");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("528029394149572618")
+setInterval(function() {
+channel.send(`Abram Ya zft`)
+}, 25)
+})
+ 
+ 
+client.login("NTE1MzQ0MzgwMDI3MDc2NjI4.DwcUtA.rHj5njIPFlt8jGERvbHg0saGAy4");
